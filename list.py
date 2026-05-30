9ad23a94b1ac294b3be1abaa527ddf8c61de67fe
Swapnil-Dubale <swapnildubale04@gmail.com>
2026-05-30 21:23:58 +0530

    DSA with python

---
+++ list.py
@@ -0,0 +1,9 @@
+print("creating List. . . .")
+
+list1 = [11, 111, "name", "name2"]
+print("List 1 = ",list1)
+
+list2 = [22, 222, "name3", "Name4"]
+print("List 2 = ",list2)
+
+print("List 1 + List 2 = ", list1 + list2)
