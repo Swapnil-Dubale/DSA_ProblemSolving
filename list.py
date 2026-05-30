d1676bbd03c7dff50018326c7de56353eeaffe00
Swapnil-Dubale <swapnildubale04@gmail.com>
2026-05-30 21:36:38 +0530

    sorting list using sorted(listName) Function

--- list.py
+++ list.py
@@ -7,3 +7,8 @@ list2 = [22, 222, "name3", "Name4"]
 print("List 2 = ",list2)
 
 print("List 1 + List 2 = ", list1 + list2)
+
+unsortedList = [11, 111, 22, 222, 5, 1]
+print("Unsorted List = ", unsortedList)
+sortedList = sorted(unsortedList)
+print("Sorted List = ", sortedList)
